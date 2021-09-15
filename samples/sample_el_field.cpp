f8663c4cac538023bf0370f63d2fe956a785d44a
MironovaEkaterina <ekaterinam1812@mail.ru>
2021-09-15 11:41:51 +0300

    change time

--- samples/sample_el_field.cpp
+++ samples/sample_el_field.cpp
@@ -3,7 +3,7 @@
 int main() {
 	double real_value;
 	Field f;
-	double dt = 1E-10;
+	double dt = 1E-11, t=1E-10;
 	Field_characteristics res;
 
 	for (int i = 0; i < f.nx; i++)
@@ -12,8 +12,8 @@ int main() {
 			f.v.B[2][i][j] = sin(2 * M_PI * (dx * i) / (bx - ax));
 		}
 
-	//for (int n = 0; n < 100; n++)
-	res = f.FDTD(dt);
+	for (double i = 0; i < t; i+=dt)
+		res = f.FDTD(dt);
 
 
 	for (int i = 0; i < 3; i++)
@@ -22,12 +22,12 @@ int main() {
 				if (i != 1)
 					real_value = 0;
 				else
-					real_value = sin(2 * M_PI * (j * dx - c * dt) / (bx - ax));
+					real_value = sin(2 * M_PI * (j * dx - c * t) / (bx - ax));
 				std::cout << "E[" << i << "][" << j << "][" << k << "]=" << res.E[i][j][k] << "  " << "E_real=" << real_value << std::endl;
 				if (i != 2)
 					real_value = 0;
 				else
-					real_value = sin(2 * M_PI * (j * dx - c * dt) / (bx - ax));
+					real_value = sin(2 * M_PI * (j * dx - c * t) / (bx - ax));
 				std::cout << "B[" << i << "][" << j << "][" << k << "]=" << res.B[i][j][k] << "  " << "B_real=" << real_value << std::endl;
 			}
 }
--- test/test_el_field.cpp
+++ test/test_el_field.cpp
@@ -6,7 +6,7 @@
 TEST(Field, right_Ex)
 {
 	Field f;
-	double dt = 1E-10, real_value,e;
+	double dt = 1E-11, t = 1E-10, real_value,e;
 	real_value = 0; 
 	Field_characteristics res;
 	for (int i = 0; i < f.nx; i++)
@@ -14,7 +14,8 @@ TEST(Field, right_Ex)
 			f.v.E[1][i][j] = sin(2 * M_PI * (dx * i) / (bx - ax));
 			f.v.B[2][i][j] = sin(2 * M_PI * (dx * i) / (bx - ax));
 		}
-	res = f.FDTD(dt);
+	for (double i = 0; i < t; i += dt)
+		res = f.FDTD(dt);
 	e = res.E[0][3][4] - real_value;
 	EXPECT_EQ(true, abs(e)<0.1);
 }
@@ -22,15 +23,16 @@ TEST(Field, right_Ex)
 TEST(Field, right_Ey)
 {
 	Field f;
-	double dt = 1E-10, real_value, e;
-	real_value = sin(2 * M_PI * (3 * dx - c * dt) / (bx - ax));
+	double dt = 1E-10, t = 1E-10, real_value, e;
+	real_value = sin(2 * M_PI * (3 * dx - c * t) / (bx - ax));
 	Field_characteristics res;
 	for (int i = 0; i < f.nx; i++)
 		for (int j = 0; j < f.ny; j++) {
 			f.v.E[1][i][j] = sin(2 * M_PI * (dx * i) / (bx - ax));
 			f.v.B[2][i][j] = sin(2 * M_PI * (dx * i) / (bx - ax));
 		}
-	res = f.FDTD(dt);
+	for (double i = 0; i < t; i += dt)
+		res = f.FDTD(dt);
 	e = res.E[1][3][4] - real_value;
 	EXPECT_EQ(true, abs(e) < 0.1);
 }
@@ -38,7 +40,7 @@ TEST(Field, right_Ey)
 TEST(Field, right_Ez)
 {
 	Field f;
-	double dt = 1E-10, real_value, e;
+	double dt = 1E-10, t = 1E-10, real_value, e;
 	real_value = 0;
 	Field_characteristics res;
 	for (int i = 0; i < f.nx; i++)
@@ -46,7 +48,8 @@ TEST(Field, right_Ez)
 			f.v.E[1][i][j] = sin(2 * M_PI * (dx * i) / (bx - ax));
 			f.v.B[2][i][j] = sin(2 * M_PI * (dx * i) / (bx - ax));
 		}
-	res = f.FDTD(dt);
+	for (double i = 0; i < t; i += dt)
+		res = f.FDTD(dt);
 	e = res.E[2][3][4] - real_value;
 	EXPECT_EQ(true, abs(e) < 0.1);
 }
@@ -54,7 +57,7 @@ TEST(Field, right_Ez)
 TEST(Field, right_Bx)
 {
 	Field f;
-	double dt = 1E-10, real_value, e;
+	double dt = 1E-10, t = 1E-10, real_value, e;
 	real_value = 0;
 	Field_characteristics res;
 	for (int i = 0; i < f.nx; i++)
@@ -62,7 +65,8 @@ TEST(Field, right_Bx)
 			f.v.E[1][i][j] = sin(2 * M_PI * (dx * i) / (bx - ax));
 			f.v.B[2][i][j] = sin(2 * M_PI * (dx * i) / (bx - ax));
 		}
-	res = f.FDTD(dt);
+	for (double i = 0; i < t; i += dt)
+		res = f.FDTD(dt);
 	e = res.B[0][3][4] - real_value;
 	EXPECT_EQ(true, abs(e) < 0.1);
 }
@@ -70,7 +74,7 @@ TEST(Field, right_Bx)
 TEST(Field, right_By)
 {
 	Field f;
-	double dt = 1E-10, real_value, e;
+	double dt = 1E-10, t = 1E-10, real_value, e;
 	real_value = 0;
 	Field_characteristics res;
 	for (int i = 0; i < f.nx; i++)
@@ -78,7 +82,8 @@ TEST(Field, right_By)
 			f.v.E[1][i][j] = sin(2 * M_PI * (dx * i) / (bx - ax));
 			f.v.B[2][i][j] = sin(2 * M_PI * (dx * i) / (bx - ax));
 		}
-	res = f.FDTD(dt);
+	for (double i = 0; i < t; i += dt)
+		res = f.FDTD(dt);
 	e = res.B[1][3][4] - real_value;
 	EXPECT_EQ(true, abs(e) < 0.1);
 }
@@ -86,15 +91,16 @@ TEST(Field, right_By)
 TEST(Field, right_Bz)
 {
 	Field f;
-	double dt = 1E-10, real_value, e;
-	real_value = sin(2 * M_PI * (3 * dx - c * dt) / (bx - ax));
+	double dt = 1E-10, t = 1E-10, real_value, e;
+	real_value = sin(2 * M_PI * (3 * dx - c * t) / (bx - ax));
 	Field_characteristics res;
 	for (int i = 0; i < f.nx; i++)
 		for (int j = 0; j < f.ny; j++) {
 			f.v.E[1][i][j] = sin(2 * M_PI * (dx * i) / (bx - ax));
 			f.v.B[2][i][j] = sin(2 * M_PI * (dx * i) / (bx - ax));
 		}
-	res = f.FDTD(dt);
+	for (double i = 0; i < t; i += dt)
+		res = f.FDTD(dt);
 	e = res.B[2][3][4] - real_value;
 	EXPECT_EQ(true, abs(e) < 0.1);
 }
