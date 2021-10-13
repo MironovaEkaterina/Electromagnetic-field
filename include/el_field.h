7a88bfb9994e081e0c900192e5572b3cf2438174
MironovaEkaterina <ekaterinam1812@mail.ru>
2021-10-13 09:44:44 +0300

    some changes

--- include/el_field.h
+++ include/el_field.h
@@ -2,10 +2,8 @@
 #include <vector>
 #include <cmath>
 
-const double dx = 1E+9;
-const double dy = 1E+10;
+
 const double c = 3E+10;
-const double ax = 0, bx = 5E+10, ay = 0, by = 5E+10;
 #define M_PI 3.1415926535897932384626433832795
 
 struct Field_characteristics {
@@ -18,44 +16,36 @@ class Field {
 public:
 	Field_characteristics v;
 	std::vector<std::vector<std::vector<double>>> J;
-	int nx = (bx - ax) / dx + 1;
-	int ny = (by - ay) / dy + 1;
+	double dx, dy;
+	int nx, ny;
+	double ax, bx, ay, by;
 
-	Field() {
-		v.E.assign(3, std::vector<std::vector<double>>(nx, std::vector<double>(ny)));
-		v.B.assign(3, std::vector<std::vector<double>>(nx, std::vector<double>(ny)));
-		J.assign(3, std::vector<std::vector<double>>(nx, std::vector<double>(ny)));
-		//начальные значения в узлах сетки и плотность тока
-		for (int k = 0; k < 3; k++) 
-			for (int i = 0; i < nx; i++)
-				for (int j = 0; j < ny; j++){
-						J[k][i][j] = 0;
-						v.E[k][i][j] = 0;
-						v.B[k][i][j] = 0;
-				}
-
+	Field(double dx_,double dy_,double ax_,double ay_,double bx_, double by_) {
+		dx = dx_;
+		dy = dy_;
+		ax = ax_;
+		bx = bx_;
+		ay = ay_;
+		by = by_;
+		nx = (bx - ax) / dx + 1;
+		ny = (by - ay) / dy + 1;
+		v.E.assign(3, std::vector<std::vector<double>>(nx, std::vector<double>(ny,0)));
+		v.B.assign(3, std::vector<std::vector<double>>(nx, std::vector<double>(ny,0)));
+		J.assign(3, std::vector<std::vector<double>>(nx, std::vector<double>(ny,0)));
 	};
 
-	Field_characteristics Field::FDTD(double dt) { //обновление сеточных значений
-
-	//обновление Е
-		for (int i = 1; i < nx - 1; i++)
-			for (int j = 1; j < ny - 1; j++) {
-				v.E[0][i][j] = v.E[0][i][j] - 4.0 * M_PI * dt * J[0][i][j] + c * dt * (v.B[2][i][j + 1] - v.B[2][i][j - 1]) / (2.0 * dy);
-				v.E[1][i][j] = v.E[1][i][j] - 4.0 * M_PI * dt * J[1][i][j] - c * dt * (v.B[2][i + 1][j] - v.B[2][i - 1][j]) / (2.0 * dx);
-				v.E[2][i][j] = v.E[2][i][j] - 4.0 * M_PI * dt * J[2][i][j] + c * dt * ((v.B[1][i + 1][j] - v.B[1][i - 1][j]) / (2.0 * dx) - (v.B[0][i][j + 1] - v.B[0][i][j - 1]) / (2.0 * dy));
-			}
-
+	void Field::border_update(double dt) { //обновление граничных значений
+		//обновление граничных значений E
 		for (int i = 0; i < nx; i++) { // j==0
 			v.E[0][i][0] = v.E[0][i][0] - 4.0 * M_PI * dt * J[0][i][0] + c * dt * (v.B[2][i][1] - v.B[2][i][ny - 1]) / (2.0 * dy);
-			if ((i != 0)&&(i!=nx-1)) {
+			if ((i != 0) && (i != nx - 1)) {
 				v.E[1][i][0] = v.E[1][i][0] - 4.0 * M_PI * dt * J[1][i][0] - c * dt * (v.B[2][i + 1][0] - v.B[2][i - 1][0]) / (2.0 * dx);
 				v.E[2][i][0] = v.E[2][i][0] - 4.0 * M_PI * dt * J[2][i][0] + c * dt * ((v.B[1][i + 1][0] - v.B[1][i - 1][0]) / (2.0 * dx) - (v.B[0][i][1] - v.B[0][i][ny - 1]) / (2.0 * dy));
 			}
-			if (i==0) {
+			if (i == 0) {
 				v.E[1][i][0] = v.E[1][i][0] - 4.0 * M_PI * dt * J[1][i][0] - c * dt * (v.B[2][i + 1][0] - v.B[2][nx - 1][0]) / (2.0 * dx);
 				v.E[2][i][0] = v.E[2][i][0] - 4.0 * M_PI * dt * J[2][i][0] + c * dt * ((v.B[1][i + 1][0] - v.B[1][nx - 1][0]) / (2.0 * dx) - (v.B[0][i][1] - v.B[0][i][ny - 1]) / (2.0 * dy));
-			} 
+			}
 			if (i == nx - 1) {
 				v.E[1][i][0] = v.E[1][i][0] - 4.0 * M_PI * dt * J[1][i][0] - c * dt * (v.B[2][0][0] - v.B[2][i - 1][0]) / (2.0 * dx);
 				v.E[2][i][0] = v.E[2][i][0] - 4.0 * M_PI * dt * J[2][i][0] + c * dt * ((v.B[1][0][0] - v.B[1][i - 1][0]) / (2.0 * dx) - (v.B[0][i][1] - v.B[0][i][ny - 1]) / (2.0 * dy));
@@ -81,23 +71,15 @@ public:
 		for (int j = 1; j < ny - 1; j++) { // i==0
 			v.E[0][0][j] = v.E[0][0][j] - 4.0 * M_PI * dt * J[0][0][j] + c * dt * (v.B[2][0][j + 1] - v.B[2][0][j - 1]) / (2.0 * dy);
 			v.E[1][0][j] = v.E[1][0][j] - 4.0 * M_PI * dt * J[1][0][j] - c * dt * (v.B[2][1][j] - v.B[2][nx - 1][j]) / (2.0 * dx);
-			v.E[2][0][j] = v.E[2][0][j] - 4.0 * M_PI * dt * J[2][0][j] + c * dt * ((v.B[1][1][j] - v.B[1][nx - 1][j]) / (2.0 * dx) - (v.B[0][0][j + 1] - v.B[0][0][j - 1]) / (2.0 * dy));  
+			v.E[2][0][j] = v.E[2][0][j] - 4.0 * M_PI * dt * J[2][0][j] + c * dt * ((v.B[1][1][j] - v.B[1][nx - 1][j]) / (2.0 * dx) - (v.B[0][0][j + 1] - v.B[0][0][j - 1]) / (2.0 * dy));
 		}
 
 		for (int j = 1; j < ny - 1; j++) { // i==nx-1
 			v.E[0][nx - 1][j] = v.E[0][nx - 1][j] - 4.0 * M_PI * dt * J[0][nx - 1][j] + c * dt * (v.B[2][nx - 1][j + 1] - v.B[2][nx - 1][j - 1]) / (2.0 * dy);
 			v.E[1][nx - 1][j] = v.E[1][nx - 1][j] - 4.0 * M_PI * dt * J[1][nx - 1][j] - c * dt * (v.B[2][0][j] - v.B[2][nx - 2][j]) / (2.0 * dx);
-			v.E[2][nx - 1][j] = v.E[2][nx - 1][j] - 4.0 * M_PI * dt * J[2][nx - 1][j] + c * dt * ((v.B[1][0][j] - v.B[1][nx - 2][j]) / (2.0 * dx) - (v.B[0][nx - 1][j + 1] - v.B[0][nx - 1][j - 1]) / (2.0 * dy));  
+			v.E[2][nx - 1][j] = v.E[2][nx - 1][j] - 4.0 * M_PI * dt * J[2][nx - 1][j] + c * dt * ((v.B[1][0][j] - v.B[1][nx - 2][j]) / (2.0 * dx) - (v.B[0][nx - 1][j + 1] - v.B[0][nx - 1][j - 1]) / (2.0 * dy));
 		}
-
-	//обновление В
-		for (int i = 1; i < nx - 1; i++)
-			for (int j = 1; j < ny - 1; j++){
-				v.B[0][i][j] = v.B[0][i][j] - c * dt * (v.E[2][i][j + 1] - v.E[2][i][j - 1]) / (2.0 * dy);
-				v.B[1][i][j] = v.B[1][i][j] + c * dt * (v.E[2][i + 1][j] - v.E[2][i - 1][j]) / (2.0 * dx);
-				v.B[2][i][j] = v.B[2][i][j] - c * dt * ((v.E[1][i + 1][j] - v.E[1][i - 1][j]) / (2.0 * dx) - (v.E[0][i][j + 1] - v.E[0][i][j - 1]) / (2.0 * dy));
-			}
-
+		//обновление граничных значений B
 		for (int i = 0; i < nx; i++) { // j==0
 			v.B[0][i][0] = v.B[0][i][0] - c * dt * (v.E[2][i][1] - v.E[2][i][ny - 1]) / (2.0 * dy);
 			if ((i != 0) && (i != nx - 1)) {
@@ -108,7 +90,7 @@ public:
 				v.B[1][i][0] = v.B[1][i][0] + c * dt * (v.E[2][i + 1][0] - v.E[2][nx - 1][0]) / (2.0 * dx);
 				v.B[2][i][0] = v.B[2][i][0] - c * dt * ((v.E[1][i + 1][0] - v.E[1][nx - 1][0]) / (2.0 * dx) - (v.E[0][i][1] - v.E[0][i][ny - 1]) / (2.0 * dy));
 			}
-			if (i == nx-1) {
+			if (i == nx - 1) {
 				v.B[1][i][0] = v.B[1][i][0] + c * dt * (v.E[2][0][0] - v.E[2][i - 1][0]) / (2.0 * dx);
 				v.B[2][i][0] = v.B[2][i][0] - c * dt * ((v.E[1][0][0] - v.E[1][i - 1][0]) / (2.0 * dx) - (v.E[0][i][1] - v.E[0][i][ny - 1]) / (2.0 * dy));
 			}
@@ -142,6 +124,24 @@ public:
 			v.B[2][nx - 1][j] = v.B[2][nx - 1][j] - c * dt * ((v.E[1][0][j] - v.E[1][nx - 2][j]) / (2.0 * dx) - (v.E[0][nx - 1][j + 1] - v.E[0][nx - 1][j - 1]) / (2.0 * dy));
 		}
 
+	};
+
+	Field_characteristics Field::FDTD(double dt) { //обновление сеточных значений
+	//обновление Е
+		for (int i = 1; i < nx - 1; i++)
+			for (int j = 1; j < ny - 1; j++) {
+				v.E[0][i][j] = v.E[0][i][j] - 4.0 * M_PI * dt * J[0][i][j] + c * dt * (v.B[2][i][j + 1] - v.B[2][i][j - 1]) / (2.0 * dy);
+				v.E[1][i][j] = v.E[1][i][j] - 4.0 * M_PI * dt * J[1][i][j] - c * dt * (v.B[2][i + 1][j] - v.B[2][i - 1][j]) / (2.0 * dx);
+				v.E[2][i][j] = v.E[2][i][j] - 4.0 * M_PI * dt * J[2][i][j] + c * dt * ((v.B[1][i + 1][j] - v.B[1][i - 1][j]) / (2.0 * dx) - (v.B[0][i][j + 1] - v.B[0][i][j - 1]) / (2.0 * dy));
+			}
+		(*this).border_update(dt);
+	//обновление В
+		for (int i = 1; i < nx - 1; i++)
+			for (int j = 1; j < ny - 1; j++){
+				v.B[0][i][j] = v.B[0][i][j] - c * dt * (v.E[2][i][j + 1] - v.E[2][i][j - 1]) / (2.0 * dy);
+				v.B[1][i][j] = v.B[1][i][j] + c * dt * (v.E[2][i + 1][j] - v.E[2][i - 1][j]) / (2.0 * dx);
+				v.B[2][i][j] = v.B[2][i][j] - c * dt * ((v.E[1][i + 1][j] - v.E[1][i - 1][j]) / (2.0 * dx) - (v.E[0][i][j + 1] - v.E[0][i][j - 1]) / (2.0 * dy));
+			}
 		return (*this).v;
 	}
 };
--- samples/sample_el_field.cpp
+++ samples/sample_el_field.cpp
@@ -2,13 +2,15 @@
 #include <fstream>
 
 int main() {
+	double ax = 0, bx = 5E+10, ay = 0, by = 5E+10;
 	double real_value;
-	Field f;
-	double dt = 0.001, t=1;
+	double dt = 0.001, t=1, dx = 1E+9, dy = 1E+10;
+	Field f(dx, dy,ax,ay,bx,by);
 	Field_characteristics res;
 
-	/*std::ofstream out("D:\\final_el_field\\Electromagnetic-field\\result.txt", std::ios_base::out | std::ios_base::trunc);
+	std::ofstream out("D:\\final_el_field\\Electromagnetic-field\\result.txt", std::ios_base::out | std::ios_base::trunc);
 	std::ofstream out2("D:\\final_el_field\\Electromagnetic-field\\result_real.txt", std::ios_base::out | std::ios_base::trunc);
+	//std::ofstream out3("D:\\final_el_field\\Electromagnetic-field\\begin.txt", std::ios_base::out | std::ios_base::trunc);
 
 	for (int i = 0; i < f.nx; i++)
 		for (int j = 0; j < f.ny; j++) {
@@ -16,12 +18,14 @@ int main() {
 			f.v.B[2][i][j] = sin(2.0 * M_PI * (dx * i) / (bx - ax));
 		}
 
+
 	for (double i = 0; i < t; i += dt) 
 		res = f.FDTD(dt);
 
 	for (int j = 0; j < f.nx; j++) {
 		out << res.E[1][j][3] << ';' << j * dx + ax << std::endl;
 		out2 << sin(2.0 * M_PI * (j * dx - c * t) / (bx - ax)) << ';' << j * dx + ax << std::endl;
+		//out3 << sin(2.0 * M_PI * (j * dx) / (bx - ax)) << ';' << j * dx + ax << std::endl;
 	}
 	
 
@@ -39,13 +43,13 @@ int main() {
 				else
 					real_value = sin(2.0 * M_PI * (j * dx - c * t) / (bx - ax));
 				std::cout << "B[" << i << "][" << j << "][" << k << "]=" << res.B[i][j][k] << "  " << "B_real=" << real_value << std::endl;
-			}*/
+			}
 
 
-	for (int i = 0; i < f.nx; i++)
+	/*for (int i = 0; i < f.nx; i++)
 		for (int j = 0; j < f.ny; j++) {
 			f.v.E[0][i][j] = sin(2.0 * M_PI * (dy * j) / (by - ay));
-			f.v.B[2][i][j] = sin(2.0 * M_PI * (dy * j) / (by - ay));
+			f.v.B[2][i][j] = -sin(2.0 * M_PI * (dy * j) / (by - ay));
 		}
 
 	for (double i = 0; i < t; i+=dt)
@@ -62,7 +66,7 @@ int main() {
 				if (i != 2)
 					real_value = 0;
 				else
-					real_value = sin(2.0 * M_PI * (k * dy - c * t) / (by - ay));
+					real_value = -sin(2.0 * M_PI * (k * dy - c * t) / (by - ay));
 				std::cout << "B[" << i << "][" << j << "][" << k << "]=" << res.B[i][j][k] << "  " << "B_real=" << real_value << std::endl;
-			}
+			}*/
 }
